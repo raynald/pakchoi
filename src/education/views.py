b9519f47eeb948230594bf9d10446b976c7938a2
Raynald Chung <raynaldchung@gmail.com>
2015-12-24 20:26:21 +0100

    search implemented, multiple columns

--- src/education/views.py
+++ src/education/views.py
@@ -1,9 +1,24 @@
 from django.views import generic
-from django.shortcuts import render
-from models import Teacher
+from django.shortcuts import render, render_to_response
+from models import Teacher, Subject
 
-# Create your views here.
 
-class SearchTeacherPage(generic.list.ListView):
-    model = Teacher
+class SearchTeacherPage(generic.ListView):
     template_name = "search_teacher.html"
+    model = Teacher
+
+    def get_queryset(self):
+        try:
+            name = self.request.GET.get('q')
+        except:
+            name = ''
+        if name != '' and name is not None:
+            object_list = self.model.objects.filter(subjects__title__icontains = name)
+        else:
+            object_list = self.model.objects.all()
+        return object_list
+
+    def get_context_data(self, **kwargs):
+        context = super(SearchTeacherPage, self).get_context_data(**kwargs)
+        context['subjects'] = Subject.objects.all()
+        return context
