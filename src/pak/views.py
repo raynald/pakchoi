fc1bad9032ae8d545f60e2a7d8ce1aa8ddc38e52
Raynald Chung <raynaldchung@gmail.com>
2015-12-24 10:06:19 +0100

    Django init project

---
+++ src/pak/views.py
@@ -0,0 +1,11 @@
+from django.views import generic
+
+
+class HomePage(generic.TemplateView):
+    template_name = "home.html"
+
+class FAQPage(generic.TemplateView):
+    template_name = "faq.html"
+
+class AboutPage(generic.TemplateView):
+    template_name = "about.html"
